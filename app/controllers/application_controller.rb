d2da10b5cc093eb1c52be7ecef42f9556e3a2bd0
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-09-05 14:42:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
   
   delete '/articles/:id/delete' do 
     article = Article.find(params[:id])
-    article.delete
+    article.destroy
     redirect '/articles'
   end
   
